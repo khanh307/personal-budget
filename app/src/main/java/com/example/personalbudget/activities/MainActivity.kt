ca64e12a772d1f81a16f2f4290d0b230a027b7d6
khanh307 <tranquockhanh307@gmail.com>
2022-11-03 14:16:44 +0700

    first commit

---
+++ app/src/main/java/com/example/personalbudget/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.personalbudget.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.personalbudget.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
